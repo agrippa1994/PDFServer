f910c50f112592fd88ba00fc6899b4bc4c36d2c3
agrippa1994 <agrippa1994@icloud.com>
2015-02-14 12:54:15 +0100

    More secure unique ids

--- unique_id.js
+++ unique_id.js
@@ -1,60 +1,61 @@
-// Use this function as a storage for the unique ids
+//	Use this function as a storage for the unique ids
 function unique_id() { }
 
-module.exports = {
-	generate: function() {
-		if(typeof unique_id.stored_ids === "undefined") {
-			unique_id.stored_ids = [];
+//	Stored ids
+unique_id.stored_ids = [];
+
+//	Static function to generate a random number between low (inclusive) and high (inclusive)
+unique_id.generator = function(low, high) {
+	return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+//	Static function to generate a string
+unique_id.generate_string = function() {
+	var str = "";
+	for(var i = 0; i < 32; i++) {
+		switch(unique_id.generator(0,2)){
+		case 0: //	ASCII 0 - 9
+			str += String.fromCharCode(unique_id.generator(0x30, 0x39));
+			break;
+		case 1: //	ASCII A - Z
+			str += String.fromCharCode(unique_id.generator(0x41, 0x5A));
+			break;
+		case 2: //	ASCII a - b
+			str += String.fromCharCode(unique_id.generator(0x61, 0x7A));
+			break;
 		}
-
-		if(typeof unique_id.generator === "undefined") {
-			unique_id.generator = function(low, high) {
-				return Math.floor(Math.random() * (high - low) + low);
-			}
-		}
-
-		if(typeof unique_id.generate_string === "undefined") {
-			unique_id.generate_string = function() {
-				var str = "";
-				for(var i = 0; i < 16; i++) 
-					str += String.fromCharCode(unique_id.generator(0x61, 0x7A));
-
-				return str;
-			}
-		}
-
-		if(typeof unique_id.generate_unique_string === "undefined") {
-			unique_id.generate_unique_string = function() {
-				while(true) {
-					var str = unique_id.generate_string();
-					var found = false;
-
-					for(var i = 0; i < unique_id.stored_ids; i++)
-						if(str == unique_id.stored_ids[i])
-							found = true;
-
-					if(!found)
-					{
-						unique_id.stored_ids.push(str);
-						return str;
-					}
-				}
-			}
-		}
-		return unique_id.generate_unique_string();
-	},
-
-	free: function(id) {
-		if(typeof unique_id.stored_ids === "undefined") {
-			unique_id.stored_ids = [];
-		}
-
-		var idx = unique_id.stored_ids.indexOf(id);
-		if(idx == -1)
-			return false;
-
-		return unique_id.stored_ids.splice(idx, 1).length != 0;
 	}
+	return str;
+}
+
+//	Static function to generate a unique string
+unique_id.generate_unique_string = function() {
+	while(true) {
+		var str = unique_id.generate_string();
+		var found = false;
+
+		for(var i = 0; i < unique_id.stored_ids; i++)
+			if(str == unique_id.stored_ids[i])
+				found = true;
+
+		if(!found)
+		{
+			unique_id.stored_ids.push(str);
+			return str;
+		}
+	}
+}
+
+//	Static function to free an id from the stored ids
+unique_id.free = function(id) {
+	var idx = unique_id.stored_ids.indexOf(id);
+	if(idx == -1)
+		return false;
+
+	return unique_id.stored_ids.splice(idx, 1).length != 0;
+}
+
+module.exports = {
+	generate: unique_id.generate_unique_string,
+	free : unique_id.free
 };
-
-
